3a196aba5c3bd275fd160bed7eff97605288f98d
Matthew O'Connell <pliniodesignori@gmail.com>
2017-05-27 10:32:09 -0400

    Last commit

--- Mutant_2.0.ino
+++ Mutant_2.0.ino
@@ -3,68 +3,108 @@
 #include <SPI.h>
 #include <Bounce.h>
 
+
 // GUItool: begin automatically generated code
-AudioSynthNoiseWhite     voice4n;         //xy=161.5,813.75
-AudioSynthWaveform       voice3b;      //xy=163,636.25
-AudioSynthWaveform       voice4b;      //xy=162.75,769.75
-AudioSynthWaveform       voice3a;      //xy=163.5,594.5
-AudioSynthWaveform       voice4a;      //xy=163,728.5
-AudioSynthNoiseWhite     voice3n;         //xy=164,678.75
-AudioSynthNoiseWhite     voice2n;         //xy=168.75,539.5
-AudioSynthWaveform       voice2b;      //xy=169.5,502
-AudioSynthWaveform       voice2a;      //xy=170,462.5000057220459
+AudioSynthNoiseWhite     voice4n;         //xy=164.5,960.75
+AudioSynthWaveform       voice4b;      //xy=165.75,916.75
+AudioSynthWaveform       voice4a;      //xy=166,875.5
+AudioSynthWaveform       voice3b;      //xy=168,737.25
+AudioSynthWaveform       voice3a;      //xy=168.5,695.5
+AudioSynthNoiseWhite     voice3n;         //xy=169,779.75
 AudioSynthWaveform       voice1a;        //xy=171.00000381469727,338.6666564941406
+AudioSynthNoiseWhite     voice2n;         //xy=170.75,592.5
 AudioSynthNoiseWhite     voice1n;        //xy=171.5,414.4166564941406
+AudioSynthWaveform       voice2b;      //xy=171.5,555
 AudioSynthWaveform       voice1b;        //xy=172.25000381469727,375.4166564941406
-AudioSynthWaveformDc     voice4env;            //xy=314,808
-AudioMixer4              voice4mix;         //xy=314.25,749.5
-AudioMixer4              voice3mix;         //xy=315.5,619.25
-AudioSynthWaveformDc     voice3env;            //xy=316.75,679.25
-AudioMixer4              voice2mix;         //xy=318.25,482.5
-AudioSynthWaveformDc     voice2env;            //xy=318.5,542.25
+AudioSynthWaveform       voice2a;      //xy=172,515.5000057220459
+AudioSynthWaveformDc     voice4filterenv;            //xy=304,1002
+AudioSynthWaveformDc     voice2filterenv;            //xy=309,638
+AudioSynthWaveformDc     voice3filterenv;            //xy=310,823.0000057220459
+AudioSynthWaveformDc     voice4env;            //xy=317,955
+AudioMixer4              voice4mix;         //xy=317.25,896.5
 AudioSynthWaveformDc     voice1env;      //xy=320.00000381469727,416.66665840148926
 AudioMixer4              voice1mix;      //xy=320.25,360.4166564941406
-AudioEffectMultiply      voice4multiply;      //xy=472.25,770.25
-AudioEffectMultiply      voice3multiply;      //xy=492,636.75
+AudioSynthWaveformDc     voice1filterenv;            //xy=320,463
+AudioMixer4              voice2mix;         //xy=320.25,535.5
+AudioSynthWaveformDc     voice2env;            //xy=320.5,595.25
+AudioMixer4              voice3mix;         //xy=320.5,720.25
+AudioSynthWaveformDc     voice3env;            //xy=321.75,780.25
+AudioEffectMultiply      voice4multiply;      //xy=475.25,917.25
+AudioEffectMultiply      voice2multiply;      //xy=493.75,558.25
 AudioEffectMultiply      voice1multiply; //xy=495.25,388.4166564941406
-AudioEffectMultiply      voice2multiply;      //xy=498.75,509.25
-AudioMixer4              mastermix;         //xy=796,587.75
-AudioOutputI2S           i2s1;           //xy=967.5,585.9166870117188
+AudioEffectMultiply      voice3multiply;      //xy=497,737.75
+AudioFilterStateVariable voice1filter;        //xy=658,390
+AudioFilterStateVariable voice3filter;        //xy=661,763.0000057220459
+AudioFilterStateVariable voice4filter;        //xy=661,922
+AudioFilterStateVariable voice2filter;        //xy=665,575
+AudioMixer4              mastermix;         //xy=864,663.75
+AudioOutputI2S           i2s1;           //xy=1044.5,668.9166870117188
 AudioConnection          patchCord1(voice4n, 0, voice4mix, 2);
-AudioConnection          patchCord2(voice3b, 0, voice3mix, 1);
-AudioConnection          patchCord3(voice4b, 0, voice4mix, 1);
-AudioConnection          patchCord4(voice3a, 0, voice3mix, 0);
-AudioConnection          patchCord5(voice4a, 0, voice4mix, 0);
+AudioConnection          patchCord2(voice4b, 0, voice4mix, 1);
+AudioConnection          patchCord3(voice4a, 0, voice4mix, 0);
+AudioConnection          patchCord4(voice3b, 0, voice3mix, 1);
+AudioConnection          patchCord5(voice3a, 0, voice3mix, 0);
 AudioConnection          patchCord6(voice3n, 0, voice3mix, 2);
-AudioConnection          patchCord7(voice2n, 0, voice2mix, 2);
-AudioConnection          patchCord8(voice2b, 0, voice2mix, 1);
-AudioConnection          patchCord9(voice2a, 0, voice2mix, 0);
-AudioConnection          patchCord10(voice1a, 0, voice1mix, 0);
-AudioConnection          patchCord11(voice1n, 0, voice1mix, 2);
-AudioConnection          patchCord12(voice1b, 0, voice1mix, 1);
-AudioConnection          patchCord13(voice4env, 0, voice4multiply, 1);
-AudioConnection          patchCord14(voice4mix, 0, voice4multiply, 0);
-AudioConnection          patchCord15(voice3mix, 0, voice3multiply, 0);
-AudioConnection          patchCord16(voice3env, 0, voice3multiply, 1);
-AudioConnection          patchCord17(voice2mix, 0, voice2multiply, 0);
-AudioConnection          patchCord18(voice2env, 0, voice2multiply, 1);
-AudioConnection          patchCord19(voice1env, 0, voice1multiply, 1);
-AudioConnection          patchCord20(voice1mix, 0, voice1multiply, 0);
-AudioConnection          patchCord21(voice4multiply, 0, mastermix, 3);
-AudioConnection          patchCord22(voice3multiply, 0, mastermix, 2);
-AudioConnection          patchCord23(voice1multiply, 0, mastermix, 0);
-AudioConnection          patchCord24(voice2multiply, 0, mastermix, 1);
-AudioConnection          patchCord25(mastermix, 0, i2s1, 0);
-AudioControlSGTL5000     sgtl5000_1;     //xy=798.5,450.6666946411133
+AudioConnection          patchCord7(voice1a, 0, voice1mix, 0);
+AudioConnection          patchCord8(voice2n, 0, voice2mix, 2);
+AudioConnection          patchCord9(voice1n, 0, voice1mix, 2);
+AudioConnection          patchCord10(voice2b, 0, voice2mix, 1);
+AudioConnection          patchCord11(voice1b, 0, voice1mix, 1);
+AudioConnection          patchCord12(voice2a, 0, voice2mix, 0);
+AudioConnection          patchCord13(voice4filterenv, 0, voice4filter, 1);
+AudioConnection          patchCord14(voice2filterenv, 0, voice2filter, 1);
+AudioConnection          patchCord15(voice3filterenv, 0, voice3filter, 1);
+AudioConnection          patchCord16(voice4env, 0, voice4multiply, 1);
+AudioConnection          patchCord17(voice4mix, 0, voice4multiply, 0);
+AudioConnection          patchCord18(voice1env, 0, voice1multiply, 1);
+AudioConnection          patchCord19(voice1mix, 0, voice1multiply, 0);
+AudioConnection          patchCord20(voice1filterenv, 0, voice1filter, 1);
+AudioConnection          patchCord21(voice2mix, 0, voice2multiply, 0);
+AudioConnection          patchCord22(voice2env, 0, voice2multiply, 1);
+AudioConnection          patchCord23(voice3mix, 0, voice3multiply, 0);
+AudioConnection          patchCord24(voice3env, 0, voice3multiply, 1);
+AudioConnection          patchCord25(voice4multiply, 0, voice4filter, 0);
+AudioConnection          patchCord26(voice2multiply, 0, voice2filter, 0);
+AudioConnection          patchCord27(voice1multiply, 0, voice1filter, 0);
+AudioConnection          patchCord28(voice3multiply, 0, voice3filter, 0);
+AudioConnection          patchCord29(voice1filter, 0, mastermix, 0);
+AudioConnection          patchCord30(voice3filter, 0, mastermix, 2);
+AudioConnection          patchCord31(voice4filter, 0, mastermix, 3);
+AudioConnection          patchCord32(voice2filter, 0, mastermix, 1);
+AudioConnection          patchCord33(mastermix, 0, i2s1, 0);
+AudioControlSGTL5000     sgtl5000_1;     //xy=1039.5,613.6666870117188
 // GUItool: end automatically generated code
 
 
 
+
+// Sets constants for Knobs and Sliders
+const int KNOB1 = A21;
+const int KNOB2 = A19;
+const int KNOB3 = A18;
+const int KNOB4 = A6;
+const int KNOB5 = A7;
+const int KNOB6 = A2;
+const int KNOB7 = A3;
+const int SLIDER1 = A15;
+const int SLIDER2 = A14;
+const int SLIDER3 = A12;
+const int SLIDER4 = A13;
+const int SWITCHLEFTBOTTOM = 30;
+const int SWITCHLEFTMIDDLE = 29;
+const int SWITCHLEFTTOP = 7;
+const int SWITCHRIGHTBOTTOM = 28;
+const int SWITCHRIGHTMIDDLE = 27;
+const int SWITCHRIGHTTOP = 26;
+int knob1, knob2, knob3, knob4, knob5, knob6, knob7;
+
+
+
+// Initialize Buttons and Note Pins
+const int notePins[4] = {0,1,24,25};
+const int indicatorLeds[4] = {6,2,8,4};
+int btnState[4], prevBtnState[4];
 // Initialize Buttons
-int notePins[4] = {0,1,24,25};
-int btnState[4];
-int prevBtnState[4];
-int indicatorLeds[4] = {6,2,8,4};
 Bounce noteBounce[] = {
   Bounce(notePins[0],10),
   Bounce(notePins[1],10),
@@ -72,8 +112,7 @@ Bounce noteBounce[] = {
   Bounce(notePins[3],10)
 };
 
-
-// Initializae Rotary Switches
+// Initialize Rotary Switches
 const int ROTARY_REFRESH_RATE = 50;
 const int TONEBANK_ROTARY_PIN = A17;
 const int TRANSPOSE_ROTARY_PIN = A16;
@@ -81,121 +120,60 @@ long toneBankLastChecked = 0;
 long transposeLastChecked = 0;
 int newScale;
 
-
-
-
 //Oscillator Intervals and Ranges
 int oscillator_octave = 2;
 int oscillator_range = 5;
 int oscillator_interval = 5; // in half steps (5 => 4ths, 7 => 5ths)
 int slider1, slider2, slider3, slider4;
 
-
-
-
 // Initialize the Tonebank variables
-int tonebank[6][2] = {  
+const int tonebank[6][2] = {  
    {WAVEFORM_SINE, WAVEFORM_SINE},
-   {WAVEFORM_TRIANGLE, WAVEFORM_TRIANGLE},
-   {WAVEFORM_SAWTOOTH_REVERSE, WAVEFORM_SAWTOOTH_REVERSE},
-   {WAVEFORM_SAWTOOTH, WAVEFORM_SAWTOOTH},
-   {WAVEFORM_PULSE, WAVEFORM_PULSE},
-   {WAVEFORM_SQUARE, WAVEFORM_SQUARE} 
+   {WAVEFORM_TRIANGLE, WAVEFORM_SAWTOOTH},
+   {WAVEFORM_TRIANGLE, WAVEFORM_SAWTOOTH_REVERSE},
+   {WAVEFORM_SQUARE, WAVEFORM_SQUARE},
+   {WAVEFORM_SAWTOOTH, WAVEFORM_SQUARE},
+   {WAVEFORM_SQUARE, WAVEFORM_SAWTOOTH_REVERSE} 
 };
-int oldTonebankNumber, tonebankNumber;
-
+int oldTonebankNumber, tonebankNumber, voiceaWaveform, voicebWaveform;
 
 // Initialize Scales and Waveforms
 // C C# D D# E F F# G G# A A# B
 const float scales[12] ={16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87};
 int oldScale = scales[0];
 
-
-
-
-
-
-
-int attackTime = 1000;
-int decayTime = 1000;
-float sustainLevel = 1;
-int releaseTime = 4000;
+// Initialize Envelope
+int droneMode;
+int attackTime, decayTime, releaseTime;
+float sustainLevel;
 bool noteTrigFlag[4];
 unsigned long attackWait[4];
+int oldNoiseLevel, oldFilterFrequency;
+
 
 
 void setup() {
-
-  AudioMemory(160);
-  Serial.begin(115200);
-  sgtl5000_1.enable();
-  sgtl5000_1.volume(.8);
-  
   initializePins();
   flashRebootLights(60);
 
-  initalizeOscillators();
+  initializeTeensyAudio();
+  initializeOscillators();
   initializeMixers();
 }
 
 void loop() {
-
-
-
-  updateSliders();
-
-
-  // TONEBANK ROTARY CHECK
-  if( toneBankLastChecked == 0 ) {
-    toneBankLastChecked = millis() + ROTARY_REFRESH_RATE;
-  }
-  if( millis() - toneBankLastChecked > ROTARY_REFRESH_RATE ) {
-
-    // We are returning 4,5,6,7,8,9 from the rotary switch, so just subtract 4 from each result to get 0 - 5
-    tonebankNumber = rotaryTurned(TONEBANK_ROTARY_PIN) - 4;
-
-    int voiceaWaveform = tonebank[tonebankNumber][0];
-    int voicebWaveform = tonebank[tonebankNumber][1];
-    
-
-    if ( oldTonebankNumber != tonebankNumber ) {
-
-      Serial.println(tonebankNumber);
-
-      voice1a.begin( voiceaWaveform );
-      voice2a.begin( voiceaWaveform );
-      voice3a.begin( voiceaWaveform );
-      voice4a.begin( voiceaWaveform );
-      
-      voice1b.begin( voicebWaveform );
-      voice2b.begin( voicebWaveform );
-      voice3b.begin( voicebWaveform );
-      voice4b.begin( voicebWaveform );
-    }
-
-    oldTonebankNumber = tonebankNumber;
-
-    toneBankLastChecked = millis();
-  }
-
-  
-
-
-  // Play / Stop Note Loop
-  for(int i=0; i<4; i++){
-    btnState[i] = digitalRead(notePins[i]);
-    if (noteBounce[i].update()){
-        if (btnState[i] == LOW && prevBtnState[i] == HIGH){
-          startNote(i);
-        }else {
-          stopNote(i);
-        }
-    }
-    prevBtnState[i] = btnState[i];
-  }
-
-
-
-
-
+  updateKnobs(); // 7 Rotary Pots
+  updateSliders(); // 4 Slider Pots
+  updateTranspose(); // Left Rotary Switch
+  updateTonebank(); // Right Rotary Switch
+  updateDroneMode(); // Left Toggle Switch
+  updateEnvelopeMode(); // Right Toggle Switch
+  updateKeys(); // 4 Momentary Switches
 }
+
+
+
+
+
+
+
---
+++ Synth_Functions.ino
@@ -0,0 +1,300 @@
+void initializePins() {
+  // INDICATOR LEDS
+  pinMode(indicatorLeds[0], OUTPUT);
+  pinMode(indicatorLeds[1], OUTPUT);
+  pinMode(indicatorLeds[2], OUTPUT);
+  pinMode(indicatorLeds[3], OUTPUT);
+
+  // NOTE BUTTONS
+  pinMode(notePins[0], INPUT_PULLUP);
+  pinMode(notePins[1], INPUT_PULLUP);
+  pinMode(notePins[2], INPUT_PULLUP);
+  pinMode(notePins[3], INPUT_PULLUP);
+
+  // TOGGLE SWITCHES
+  pinMode(7, INPUT_PULLUP);
+  pinMode(29, INPUT_PULLUP);
+  pinMode(30, INPUT_PULLUP);
+  pinMode(26, INPUT_PULLUP);
+  pinMode(27, INPUT_PULLUP);
+  pinMode(28, INPUT_PULLUP);
+  
+}
+
+// This goes in the setup sketch to initialize the audio board
+void initializeTeensyAudio() {
+  AudioMemory(160);
+  Serial.begin(115200);
+  sgtl5000_1.enable();
+  sgtl5000_1.volume(.8);
+}
+
+void flashRebootLights(int speed) {
+  // Turn off Teensy LED
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  
+  int i = 0;
+  while(i < 5) {
+    digitalWrite(indicatorLeds[0], HIGH);
+    delay(speed);
+    digitalWrite(indicatorLeds[0], LOW);
+    digitalWrite(indicatorLeds[1], HIGH);
+    delay(speed);
+    digitalWrite(indicatorLeds[1], LOW);
+    digitalWrite(indicatorLeds[2], HIGH);
+    delay(speed);
+    digitalWrite(indicatorLeds[2], LOW);
+    digitalWrite(indicatorLeds[3], HIGH);
+    delay(speed);
+    digitalWrite(indicatorLeds[3], LOW);
+    i++;
+  }
+}
+
+// Turn on the waveforms - This is arbitrary since it will be changed in the loop
+void initializeOscillators() {
+  voice1a.begin(.3,1,WAVEFORM_SQUARE);
+  voice1b.begin(.3,1,WAVEFORM_SQUARE);
+
+  voice2a.begin(.3,1,WAVEFORM_SQUARE);
+  voice2b.begin(.3,1,WAVEFORM_SQUARE);
+
+  voice3a.begin(.3,1,WAVEFORM_SQUARE);
+  voice3b.begin(.3,1,WAVEFORM_SQUARE);
+
+  voice4a.begin(.3,1,WAVEFORM_SQUARE);
+  voice4b.begin(.3,1,WAVEFORM_SQUARE);
+
+
+}
+
+// Turn on the Mixers
+void initializeMixers() {
+  voice1mix.gain(0,.5);
+  voice1mix.gain(1,.5);
+  voice1mix.gain(2,.5);
+
+  voice2mix.gain(0,.5);
+  voice2mix.gain(1,.5);
+  voice2mix.gain(2,.5);
+
+  voice3mix.gain(0,.5);
+  voice3mix.gain(1,.5);
+  voice3mix.gain(2,.5);
+
+  voice4mix.gain(0,.5);
+  voice4mix.gain(1,.5);
+  voice4mix.gain(2,.5);
+
+  mastermix.gain(0,.5);
+  mastermix.gain(1,.5);
+  mastermix.gain(2,.5);
+  mastermix.gain(3,.5);
+}
+
+
+
+void startNote(int i) {
+  noteTrigFlag[i] = false;
+
+  if (i == 0) {
+    voice1filterenv.amplitude(-1, releaseTime);
+    voice1env.amplitude(0,releaseTime);
+  } else if (i == 1) {
+    voice2filterenv.amplitude(-1, releaseTime);
+    voice2env.amplitude(0,releaseTime);
+  } else if (i == 2) {
+    voice3filterenv.amplitude(-1, releaseTime);
+    voice3env.amplitude(0,releaseTime);
+  } else if (i == 3) {
+    voice4filterenv.amplitude(-1, releaseTime);
+    voice4env.amplitude(0,releaseTime);
+  }   
+}
+
+void stopNote(int i) {  
+  noteTrigFlag[i] = true;
+  attackWait[i] = millis();
+
+  if (i == 0) {
+    voice1filterenv.amplitude(1,attackTime);
+    voice1env.amplitude(1,attackTime);
+  } else if (i == 1) {
+    voice2filterenv.amplitude(1,attackTime);
+    voice2env.amplitude(1,attackTime);
+  } else if (i == 2) {
+    voice3filterenv.amplitude(1,attackTime);
+    voice3env.amplitude(1,attackTime);
+  } else if (i == 3) {
+    voice4filterenv.amplitude(1,attackTime);
+    voice4env.amplitude(1,attackTime);
+  }   
+}
+
+void updateKnobs() {
+  knob1 = analogRead(KNOB1);
+  knob2 = analogRead(KNOB2);
+  knob3 = analogRead(KNOB3);
+  knob4 = analogRead(KNOB4);
+  knob5 = analogRead(KNOB5);
+  knob6 = analogRead(KNOB6);
+  knob7 = analogRead(KNOB7);
+
+
+//  updateFilter(knob1);
+  updateNoise(knob2);
+
+
+}
+
+
+void updateFilter(int filterFrequency) {
+  if (filterFrequency > oldFilterFrequency + 20 || filterFrequency < oldFilterFrequency - 20) {
+
+    int filterFrequencyMapped = map(filterFrequency, 0, 1023, 0, 22000);
+  
+    Serial.println(filterFrequencyMapped);
+    
+    voice1filter.frequency(filterFrequencyMapped);
+    voice2filter.frequency(filterFrequencyMapped);
+    voice3filter.frequency(filterFrequencyMapped);
+    voice4filter.frequency(filterFrequencyMapped);
+
+    oldFilterFrequency = filterFrequency;
+  }
+
+}
+
+void updateNoise(int noiseLevel) {
+  if (noiseLevel != oldNoiseLevel) {
+    float noiseLevelMapped = mapfloat(noiseLevel, 0, 1023, 0, .3);
+    
+//    Serial.println(noiseLevelMapped);
+  
+    voice1n.amplitude(noiseLevelMapped);
+    voice2n.amplitude(noiseLevelMapped);
+    voice3n.amplitude(noiseLevelMapped);
+    voice4n.amplitude(noiseLevelMapped);
+    
+    oldNoiseLevel = noiseLevel;
+  }
+}
+
+
+
+void updateSliders() {
+  slider1 = analogRead(SLIDER1);
+  slider2 = analogRead(SLIDER2);
+  slider3 = analogRead(SLIDER3);
+  slider4 = analogRead(SLIDER4);
+
+  // Map the sliders to specific starting notes and intervals
+  float osc1freq = mapfloat(slider1, 0, 1023, scales [ ((newScale) % 12) ] * pow(2, oscillator_octave + newScale / 12), scales [ ((newScale + oscillator_range) % 12) ] * pow(2, oscillator_octave + (newScale + oscillator_range) / 12));
+  float osc2freq = mapfloat(slider2, 0, 1023, scales [ ((newScale + oscillator_interval) % 12) ] * pow(2, oscillator_octave + (newScale + oscillator_interval) / 12), scales [ ((newScale + oscillator_interval + oscillator_range) % 12) ] * pow(2, oscillator_octave + (newScale + oscillator_interval + oscillator_range) / 12));
+  float osc3freq = mapfloat(slider3, 0, 1023, scales [ ((newScale + oscillator_interval * 2) % 12) ] * pow(2, oscillator_octave + (newScale + oscillator_interval * 2) / 12), scales [ ((newScale + oscillator_interval * 2 + oscillator_range) % 12) ] * pow(2, oscillator_octave + (newScale + oscillator_interval * 2 + oscillator_range) / 12));
+  float osc4freq = mapfloat(slider4, 0, 1023, scales [ ((newScale + oscillator_interval * 3) % 12) ] * pow(2, oscillator_octave + (newScale + oscillator_interval * 3) / 12), scales [ ((newScale + oscillator_interval * 3 + oscillator_range) % 12) ] * pow(2, oscillator_octave + (newScale + oscillator_interval * 3 + oscillator_range) / 12));
+
+  voice1a.frequency(osc1freq);
+  voice2a.frequency(osc2freq);
+  voice3a.frequency(osc3freq);
+  voice4a.frequency(osc4freq);
+
+  voice1b.frequency(osc1freq);
+  voice2b.frequency(osc2freq);
+  voice3b.frequency(osc3freq);
+  voice4b.frequency(osc4freq);
+}
+
+
+void updateTonebank() {
+  // TONEBANK ROTARY CHECK
+  if( toneBankLastChecked == 0 ) {
+    toneBankLastChecked = millis() + ROTARY_REFRESH_RATE;
+  }
+  if( millis() - toneBankLastChecked > ROTARY_REFRESH_RATE ) {
+
+    // We are returning 4,5,6,7,8,9 from the rotary switch, so just subtract 4 from each result to get 0 - 5
+    tonebankNumber = rotaryTurned(TONEBANK_ROTARY_PIN) - 4;
+
+    voiceaWaveform = tonebank[tonebankNumber][0];
+    voicebWaveform = tonebank[tonebankNumber][1];
+    
+    if ( oldTonebankNumber != tonebankNumber ) {
+      Serial.println(tonebankNumber);
+      voice1a.begin( voiceaWaveform );
+      voice2a.begin( voiceaWaveform );
+      voice3a.begin( voiceaWaveform );
+      voice4a.begin( voiceaWaveform );
+      
+      voice1b.begin( voicebWaveform );
+      voice2b.begin( voicebWaveform );
+      voice3b.begin( voicebWaveform );
+      voice4b.begin( voicebWaveform );
+    }
+    oldTonebankNumber = tonebankNumber;
+    toneBankLastChecked = millis();
+  }
+}
+
+void updateTranspose() {
+  // TRANSPOSE ROTARY CHECK
+  if( transposeLastChecked == 0 ) {
+    transposeLastChecked = millis() + ROTARY_REFRESH_RATE; 
+  }
+  if( millis() - transposeLastChecked > ROTARY_REFRESH_RATE ) {
+    newScale = rotaryTurned(TRANSPOSE_ROTARY_PIN);
+    oldScale = newScale;
+    transposeLastChecked = millis();
+  }
+}
+
+void updateEnvelopeMode() {
+  if (digitalRead(SWITCHLEFTBOTTOM) == LOW) {
+    attackTime = 100;
+    decayTime = 100;
+    sustainLevel = 1;
+    releaseTime = 200;
+  } else if (digitalRead(SWITCHLEFTMIDDLE) == LOW) {
+    attackTime = 100;
+    decayTime = 100;
+    sustainLevel = 1;
+    releaseTime = 1500;  
+  } else if (digitalRead(SWITCHLEFTTOP) == LOW) {
+    attackTime = 1000;
+    decayTime = 100;
+    sustainLevel = 1;
+    releaseTime = 3000; 
+  }
+}
+
+void updateDroneMode() {
+  if (digitalRead(SWITCHRIGHTBOTTOM) == LOW) {
+    oscillator_interval = 5;
+    oscillator_range = 5;
+  }
+  if (digitalRead(SWITCHRIGHTMIDDLE) == LOW) {
+    oscillator_interval = 5;
+    oscillator_range = 12;
+  }
+  if (digitalRead(SWITCHRIGHTTOP) == LOW) {
+    oscillator_interval = 7;
+    oscillator_range = 5;
+  }
+}
+
+void updateKeys() {
+  for(int i=0; i<4; i++){
+    btnState[i] = digitalRead(notePins[i]);
+    if (noteBounce[i].update()){
+        if (btnState[i] == LOW && prevBtnState[i] == HIGH){
+          startNote(i);
+        }else {
+          stopNote(i);
+        }
+    }
+    prevBtnState[i] = btnState[i];
+  }
+}
+
+
